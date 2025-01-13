8cda97c6ef3caf1d06fb7e20caabc89222f09081
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2025-01-13 15:15:18 -0800

    ci(test-spcs): add command stubs to create a test service in SPCS

--- utils/devshell-cmds/ci.nix
+++ utils/devshell-cmds/ci.nix
@@ -19,6 +19,39 @@ in
           --platform=linux/amd64 ${settings.imageTag}
       '';
   }
+  # The test compute pool should be created independently
+  # This command will start the compute pool and block until it's up
+  # If the compute pool is already up -- the command will short-circuit to success
+  {
+    help = "Make sure test compute pool is started";
+    name = "snowcli-start-pool-wait-until-started";
+    command =
+      # bash
+      ''
+        exit 1
+      '';
+  }
+  # Creates SPCS service to perform tests against
+  # Blocks until service is up or times out
+  {
+    help = "Set up a temporary service on the test service";
+    name = "snowcli-create-test-service";
+    command =
+      # bash
+      ''
+        exit 1
+      '';
+  }
+  # Retrieves the temporary service endpoint to be used for tests
+  {
+    help = "Get the temporary service endpoint";
+    name = "snowcli-get-test-service-url";
+    command =
+      # bash
+      ''
+        exit 1
+      '';
+  }
 ]
 # Add category
 |> map (cmd: cmd // { inherit (settings) category; })
