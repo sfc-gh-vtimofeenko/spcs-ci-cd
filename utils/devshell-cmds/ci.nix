dc83ed70ffa7a204ea1dbae28aa544fc8a81816e
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2025-01-16 14:03:13 -0800

    ci(flake): strealmine the ci.nix code

--- utils/devshell-cmds/ci.nix
+++ utils/devshell-cmds/ci.nix
@@ -7,54 +7,59 @@
 let
   settings.category = "ci";
   inherit (pkgs) lib;
+
+  /**
+    Function that produces a devshell commands.
+    All these commands are basically wrappers around bash utilities that ensure
+    runtimeInputs are present.
+  */
+  mkCmd =
+    {
+      help,
+      name,
+      runtimeInputs,
+    }:
+    {
+      inherit help name;
+      command =
+        {
+          inherit name runtimeInputs;
+          text = builtins.readFile (./../. + "/${name}");
+        }
+        |> pkgs.writeShellApplication
+        |> lib.getExe;
+    };
 in
 [
-  rec {
+
+  {
     help = "Run docker in GH action";
     name = "docker-run";
-    command =
-      {
-        inherit name;
-        runtimeInputs = [ ];
-        text = builtins.readFile (./../. + "/${name}");
-      }
-      |> pkgs.writeShellApplication
-      |> lib.getExe;
+    runtimeInputs = [ ];
   }
-  rec {
+
+  {
     help = "Push the docker archive into Snowflake";
     name = "skopeo-push-to-snowflake-registry";
-    command =
-      {
-        inherit name;
-        runtimeInputs = [
-          pkgs.snowflake-cli # for `snow`
-          pkgs.skopeo # for `skopeo`
-        ];
-        text = builtins.readFile ../skopeo-push-to-snowflake-registry;
-      }
-      |> pkgs.writeShellApplication
-      |> lib.getExe;
+    runtimeInputs = [
+      pkgs.snowflake-cli # for `snow`
+      pkgs.skopeo # for `skopeo`
+    ];
   }
+
   # The test compute pool should be created independently
   # This command will start the compute pool and block until it's up
   # If the compute pool is already up -- the command will short-circuit to success
-  rec {
+  {
     help = "Make sure test compute pool is started";
     name = "start-test-compute-pool-wait-until-up";
-    command =
-      {
-        inherit name;
-        runtimeInputs = [
-          pkgs.snowflake-cli # `needed for snow`
-          pkgs.jq
-          pkgs.coreutils-full # needed for `timeout`
-        ];
-        text = builtins.readFile (./../. + "/${name}");
-      }
-      |> pkgs.writeShellApplication
-      |> lib.getExe;
+    runtimeInputs = [
+      pkgs.snowflake-cli # `needed for snow`
+      pkgs.jq
+      pkgs.coreutils-full # needed for `timeout`
+    ];
   }
+
   # Inverse of the previous command
   {
     help = "Stop the test compute pool";
@@ -66,21 +71,14 @@ in
 
   # Creates SPCS service to perform tests against
   # Blocks until service is up or times out
-  rec {
+  {
     help = "Set up a temporary service on the test service";
     name = "create-test-service-wait-until-up";
-    command =
-      {
-        inherit name;
-        runtimeInputs = [
-          pkgs.snowflake-cli # `needed for snow`
-          pkgs.jq
-          pkgs.coreutils-full # needed for `timeout`
-        ];
-        text = builtins.readFile (./../. + "/${name}");
-      }
-      |> pkgs.writeShellApplication
-      |> lib.getExe;
+    runtimeInputs = [
+      pkgs.snowflake-cli # `needed for snow`
+      pkgs.jq
+      pkgs.coreutils-full # needed for `timeout`
+    ];
   }
 
   {
@@ -89,39 +87,28 @@ in
     command = "snow spcs service describe $TEST_SERVICE_DB.$TEST_SERVICE_SCHEMA.$TEST_SERVICE_NAME";
   }
 
-  rec {
+  {
     help = "Run the test suite against the spcs test service";
     name = "run-tests-against-spcs";
-    command =
-      {
-        inherit name;
-        runtimeInputs = [
-          pkgs.snowflake-cli # `needed for snow`
-          pkgs.jq
-          pkgs.hurl
-          pkgs.git
-        ];
-        text = builtins.readFile (./../. + "/${name}");
-      }
-      |> pkgs.writeShellApplication
-      |> lib.getExe;
+    runtimeInputs = [
+      pkgs.snowflake-cli # `needed for snow`
+      pkgs.jq
+      pkgs.hurl
+      pkgs.git
+    ];
   }
 
-  rec {
+  {
     help = "Tear down service";
     name = "tear-down-service";
-    command =
-      {
-        inherit name;
-        runtimeInputs = [
-          pkgs.snowflake-cli # `needed for snow`
-        ];
-        text = builtins.readFile (./../. + "/${name}");
-      }
-      |> pkgs.writeShellApplication
-      |> lib.getExe;
+    runtimeInputs = [
+      pkgs.snowflake-cli # `needed for snow`
+    ];
   }
 ]
+# If there is an explicit 'command' attr, then the command is already in expected format
+# Otherwise turn it into command
+|> map (cmd: if builtins.hasAttr "command" cmd then cmd else mkCmd cmd)
 # Add category
 |> map (cmd: cmd // { inherit (settings) category; })
 # Add prefix to command
